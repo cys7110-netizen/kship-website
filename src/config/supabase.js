d7661d6b88d7fed056dfcd600ffa4743f1c84af4
cys7110-netizen <cys7110@gmail.com>
2026-06-12 07:16:01 +0900

    0715

--- src/config/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://zejkbveigebqiiwvosrd.supabase.co';
-const SUPABASE_KEY = 'sb_publishable__R1mWzUGZZQKrBT4L_1RhA_UC6vRa6E';
-
-export const sb = createClient(SUPABASE_URL, SUPABASE_KEY);
